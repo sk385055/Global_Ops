57d284906d1b5b8e2b199d279f3c30edd4813e42
sk385055 <sk385055@ncratleos.com>
2025-12-28 11:58:35 -0500

    Add files via upload

---
+++ schedule Stability - Weekly & Monthly/Juypter Prompt.bat
@@ -0,0 +1,6 @@
+@echo off
+REM Activate the base environment
+call E:\softwares\anaconda3\condabin\activate base
+
+REM Start Jupyter Notebook in the specified directory
+start "" jupyter notebook --notebook-dir="E:/_Projects/Schedule Stability"
