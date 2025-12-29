9136fb3c546e8484e5a83b8151ef2cfa1f005a91
sk385055 <sk385055@ncratleos.com>
2025-12-28 22:37:49 -0500

    Add files via upload

---
+++ F-BOM Clubbing/Juypter.bat
@@ -0,0 +1,7 @@
+
+@echo off
+REM Activate the base environment
+call E:\softwares\anaconda3\condabin\activate base
+
+REM Start Jupyter Notebook in the specified directory
+start "" jupyter notebook --notebook-dir="E:/_Projects/FBOM"
