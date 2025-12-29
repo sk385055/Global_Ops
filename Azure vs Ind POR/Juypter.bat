c3dec03194c4afb5a99c7bcb88b634ac7b6dca63
sk385055 <sk385055@ncratleos.com>
2025-12-28 22:35:41 -0500

    Add files via upload

---
+++ Azure vs Ind POR/Juypter.bat
@@ -0,0 +1,10 @@
+@echo off
+explorer "E:\_Projects\DEP POR\DEP POR\Input"
+
+
+@echo off
+REM Activate the base environment
+call E:\softwares\anaconda3\condabin\activate base
+
+REM Start Jupyter Notebook in the specified directory
+start "" jupyter notebook --notebook-dir="E:/_Projects/DEP POR/Azure Vs IND POR"
